4ff0414e6acb0ec41058e1b5070ab4eef430ddda
Patrick Mattei <patrick.mattei@gmail.com>
2018-02-08 22:15:31 -0500

    Styled About Page and linked to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
 	return (
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the About Page</p>
+			<h1 className="text-center">About</h1>
+			<p>This page was created by Patrick Mattei as part of the Udemy React Web Dev Course.<br />Check it out on <a href="https://github.com/PatMattei/Udemy-React-WeatherApp">Github</a>.</p>
+			<p>Here are some resources I used:</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a> - This was the Javascript framework used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search weather data by city name.
+				</li>
+			</ul>
 		</div>
 	)
 };
